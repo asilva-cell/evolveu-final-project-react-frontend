fc4a87a159b7fc6b3984ea14060a423e7665259a
Adriana Silva <achapellin@gmail.com>
2020-01-29 23:10:47 -0700

    responsive for smaller screens

---
+++ build/precache-manifest.8983531797533eaa625ffb6b5e25fe6e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da48a882a43d5f7bfcca657b05dd869e",
+    "url": "/evolveu-final-project-react-frontend/index.html"
+  },
+  {
+    "revision": "e7b8e7c37b336e6f3004",
+    "url": "/evolveu-final-project-react-frontend/static/css/main.b6f817da.chunk.css"
+  },
+  {
+    "revision": "9150bada207c99faa74a",
+    "url": "/evolveu-final-project-react-frontend/static/js/2.83dd1c1b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/evolveu-final-project-react-frontend/static/js/2.83dd1c1b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e7b8e7c37b336e6f3004",
+    "url": "/evolveu-final-project-react-frontend/static/js/main.94e09b26.chunk.js"
+  },
+  {
+    "revision": "c514486351e5812a898e",
+    "url": "/evolveu-final-project-react-frontend/static/js/runtime-main.5f4bfdd2.js"
+  },
+  {
+    "revision": "b889b2d9ad162f2edd7b5df00f727982",
+    "url": "/evolveu-final-project-react-frontend/static/media/arrowDouble.b889b2d9.svg"
+  },
+  {
+    "revision": "1c221b13c9f4ce4f45663e65f71cf969",
+    "url": "/evolveu-final-project-react-frontend/static/media/arrowSingle.1c221b13.svg"
+  },
+  {
+    "revision": "3204090b7e9c239050054ab4ac0929cc",
+    "url": "/evolveu-final-project-react-frontend/static/media/city.3204090b.svg"
+  },
+  {
+    "revision": "9a893aa5d67b9d0a51746ff4b5ec1df0",
+    "url": "/evolveu-final-project-react-frontend/static/media/contact.9a893aa5.svg"
+  },
+  {
+    "revision": "62fd842e93ed809d63ad3f74dc23797b",
+    "url": "/evolveu-final-project-react-frontend/static/media/gameBackground.62fd842e.jpg"
+  },
+  {
+    "revision": "ea5a45b949a1fc33cc7e9f9a38a50d8f",
+    "url": "/evolveu-final-project-react-frontend/static/media/link.ea5a45b9.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/evolveu-final-project-react-frontend/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "da98c0a79bbeb528650689cbbed63c88",
+    "url": "/evolveu-final-project-react-frontend/static/media/payment.da98c0a7.svg"
+  },
+  {
+    "revision": "eb6cdbc20bfc1cdef8a2dd4e75bf9069",
+    "url": "/evolveu-final-project-react-frontend/static/media/setting.eb6cdbc2.svg"
+  },
+  {
+    "revision": "bcf956bd2d2ea9762d22fa043cee048e",
+    "url": "/evolveu-final-project-react-frontend/static/media/stack.bcf956bd.svg"
+  },
+  {
+    "revision": "56a6fe9eac015b8313b0c3102c9a72f6",
+    "url": "/evolveu-final-project-react-frontend/static/media/tic-tac-toe.56a6fe9e.svg"
+  }
+]);
